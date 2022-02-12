97951c81060f656d5bb1a6e2bf96a52186a6ca3e
goreleaserbot <goreleaser@carlosbecker.com>
2022-02-12 09:43:45 +0100

    Brew formula update for lithia version v0.0.14

--- Formula/lithia.rb
+++ Formula/lithia.rb
@@ -5,13 +5,13 @@
 class Lithia < Formula
   desc ""
   homepage ""
-  version "0.0.13"
+  version "0.0.14"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/vknabel/lithia/releases/download/v0.0.13/lithia_0.0.13_Darwin_arm64.tar.gz"
-      sha256 "b49262d5705a73201b3d5cda9f22adc0547f14fe2c684b10e73018f0fe1e9ff7"
+      url "https://github.com/vknabel/lithia/releases/download/v0.0.14/lithia_0.0.14_Darwin_arm64.tar.gz"
+      sha256 "5128ccae6e6fd31f2e243b267a565f04c10ab096a1bdab3afbf0899a05ab6211"
 
       def install
         bin.install "lithia"
@@ -19,8 +19,8 @@ class Lithia < Formula
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/vknabel/lithia/releases/download/v0.0.13/lithia_0.0.13_Darwin_x86_64.tar.gz"
-      sha256 "c6492ba0bfdbab89cddaf5f79e70d3f23cc933a41222ee6b9639ad4d8e33c28f"
+      url "https://github.com/vknabel/lithia/releases/download/v0.0.14/lithia_0.0.14_Darwin_x86_64.tar.gz"
+      sha256 "f65c43ebb8f39d47d52a90088dc8e9e0dd75da6124cf50a5ab4d817a82291bc2"
 
       def install
         bin.install "lithia"
@@ -31,8 +31,8 @@ class Lithia < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/vknabel/lithia/releases/download/v0.0.13/lithia_0.0.13_Linux_arm64.tar.gz"
-      sha256 "7be959349740b490d4575e427374b963ef737824fdb2fa32b5de08e26b79df42"
+      url "https://github.com/vknabel/lithia/releases/download/v0.0.14/lithia_0.0.14_Linux_arm64.tar.gz"
+      sha256 "e6195479e1fcbb6cfbfae701dbf7fb6ecb058e0e8df27e2674abf0038cdfd5e4"
 
       def install
         bin.install "lithia"
@@ -40,8 +40,8 @@ class Lithia < Formula
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/vknabel/lithia/releases/download/v0.0.13/lithia_0.0.13_Linux_x86_64.tar.gz"
-      sha256 "1443bad80678fc123e491506e6dea99008d4fe3df52e8fba0dea34054c4c1c44"
+      url "https://github.com/vknabel/lithia/releases/download/v0.0.14/lithia_0.0.14_Linux_x86_64.tar.gz"
+      sha256 "af2a7b56562e4d30d1adeecc4e81b2d72e85327c554dc16d7ec4f825cbb328ef"
 
       def install
         bin.install "lithia"
